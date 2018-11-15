40051bb4fc06b7e360ffbef4fe085e71feefa530
Philip Shamsiev <ph.shamsiev@mail.ru>
2018-11-15 11:08:01 +0300

    Add directives to app

---
+++ videoApp/directives.js
@@ -0,0 +1,22 @@
+//DIRECTIVES
+app.directive('videoDetail', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/videoDetail.html',
+    replace: true,
+    scope: {
+      
+    }
+  }
+});
+
+app.directive('videoList', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/videoList.html',
+    replace: true,
+    scope: {
+
+    }
+  }
+});
