7b05ad98cd5d0f67400bd3b2964ed6ba56bd7cde
Philip Shamsiev <ph.shamsiev@mail.ru>
2018-11-16 10:30:47 +0300

    Add add video method; refactor show/hide form video logic

--- videoApp/controllers.js
+++ videoApp/controllers.js
@@ -6,9 +6,11 @@ app.controller('homeController', ['$scope', function ($scope) {
 app.controller('videoCenterController', ['$scope', 'videoService', function ($scope, videoService) {
   $scope.videos = [];
 
-  $scope.selectedVideo;
+  $scope.selectedVideo = null;
 
   $scope.editTitle = false;
+
+  $scope.hideNewVideoForm = true;
 
   videoService.GetVideos().then(function (data) {
     $scope.videos = data.data;
@@ -18,6 +20,7 @@ app.controller('videoCenterController', ['$scope', 'videoService', function ($sc
 
   $scope.onSelect = function(video) {
     $scope.selectedVideo = video;
+    $scope.hideNewVideoForm = true;
     videoService.currentVideo = video;
   };
 
@@ -34,18 +37,49 @@ app.controller('videoCenterController', ['$scope', 'videoService', function ($sc
   };
 
   $scope.updateVideo = function() {
-    videoService.UpdateVideos($scope.selectedVideo).then(function (data) {
-      console.log(data);
+    videoService.UpdateVideos($scope.selectedVideo).then(function (updatedVideo) {
+      console.log(updatedVideo);
+      $scope.selectedVideo = null;
     }, function (e) {
       console.log(e);
     });
   };
 
   $scope.deleteVideo = function() {
-    videoService.DeleteVideo($scope.selectedVideo).then(function (data) {
-      console.log(data);
+    videoService.DeleteVideo($scope.selectedVideo).then(function (deletedVideo) {
+      console.log(deletedVideo);
+      var videoArray = $scope.videos;
+
+      for (var i = 0; i < videoArray.length; i++) {
+        if (videoArray[i]._id === $scope.selectedVideo._id) {
+          videoArray.splice(i, 1);
+        }
+      }
+
+      $scope.selectedVideo = null;
     }, function (e) {
       console.log(e);
     });
-  }
+  };
+
+  $scope.addVideo = function(form) {
+    console.log($scope.selectedVideo);
+    var newVideo = {};
+    newVideo.title = form.title.$viewValue;
+    newVideo.url = form.url.$viewValue;
+    newVideo.description = form.description.$viewValue;
+
+    videoService.AddVideo(newVideo).then(function (addedVideo) {
+      $scope.videos.push(addedVideo);
+      $scope.selectedVideo = addedVideo.data;
+      console.log($scope.selectedVideo);
+      $scope.hideNewVideoForm = true;
+    }, function (e) {
+      console.log(e);
+    });
+  };
+
+  $scope.toggleNewVideoForm = function() {
+    $scope.hideNewVideoForm = false;
+  };
 }]);
